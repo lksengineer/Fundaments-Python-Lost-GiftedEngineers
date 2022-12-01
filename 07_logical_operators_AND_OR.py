722ba52eff410e833b812af318cd33ef1f26a480
Luis Kennedy Saavedra <ksaavedrasu@gmail.com>
2022-12-01 02:20:24 +0000

    Logical operators AND, OR, NOT

---
+++ 07_logical_operators_AND_OR.py
@@ -0,0 +1,14 @@
+a = "*" * 10
+
+print(f"\n{a}Logical operator: \"AND\"{a}")
+print(f'True and True = {True and True}')
+print(f'True and False = {True and False}')
+print(f'False and True = {False and True}')
+print(f'False and False = {False and False}')
+
+
+print(f'\n{a}Logical operator: "OR"{a}')
+print(f'True or True = {True or True}')
+print(f'True or False = {True or False}')
+print(f'False or True = {False or True}')
+print(f'False or False = {False or False}')
---
+++ 08_logical_operator_NOT.py
@@ -0,0 +1,14 @@
+a = "*" * 10
+
+print(f"\n{a}Logical operator: \"NOT AND\"{a}")
+print(f'not (True and True) = {not (True and True)}')
+print(f'not (True and False) = {not (True and False)}')
+print(f'not (False and True) = {not (False and True)}')
+print(f'not (False and False) = {not (False and False)}')
+
+
+print(f'\n{a}Logical operator: "NOT OR"{a}')
+print(f'not (True or True) = {not (True or True)}')
+print(f'not (True or False) = {not (True or False)}')
+print(f'not (False or True) = {not (False or True)}')
+print(f'not (False or False) = {not (False or False)}')
