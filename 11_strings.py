17a076b8cdb0e8ac1c0f67b1cec0e0c8d0f14f15
Luis Kennedy Saavedra <ksaavedrasu@gmail.com>
2022-12-08 22:08:24 +0000

    strings

---
+++ 11_strings.py
@@ -0,0 +1,37 @@
+text = 'Ella sabe programar en Python'
+
+'''
+print('JavaScript' in text)
+print('Python' in text)
+
+if 'JS' in text:
+  print('Elegiste bien!!')
+else:
+  print('Tambien elegiste bien')
+
+'''
+
+size = len(text)
+print(size)
+
+print(text)
+print(text.upper())
+print(text.lower())
+print(text.count('a'))
+
+print(text.swapcase())
+print(text.startswith('Ella'))
+print(text.endswith('Rust'))
+print(text.replace('Python', 'Go'))
+
+text_2 = 'este es un titulo'
+print(text_2)
+print(text_2.capitalize())
+print(text_2.title())
+print(text_2.isdigit())
+print("398".isdigit())
+print("398".isnumeric())
+print("398".isalpha())
+print("398".isalnum())
+print("3988".isdecimal())
+
